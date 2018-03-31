b51c739ce15ab15f55b2ec4bcd5a380bc6fbcbb0
cindyvitria <cindyvitria99@gmail.com>
2018-03-31 14:26:43 +0700

    membuat atribut dan behavior

--- src/tugas2/BelahKetupat.java
+++ src/tugas2/BelahKetupat.java
@@ -1,8 +1,10 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package tugas2;
 
 /**
@@ -10,5 +12,15 @@ package tugas2;
  * @author Acer
  */
 public class BelahKetupat {
+    int sisi1;
+    int sisi2;
+    int sisi3;
+    int sisi4;
     
+    void cetakinfo() {
+        System.out.println("sisi1 = "+sisi1);
+        System.out.println("sisi2 = "+sisi2);
+        System.out.println("sisi3 = "+sisi3);
+        System.out.println("sisi4 = "+sisi4);
+    }
 }
